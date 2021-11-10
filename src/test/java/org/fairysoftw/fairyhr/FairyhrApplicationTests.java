65696d4b06a8e5723638f40dc04b9b5f53c21fae
ColourfulBlack <kalicock@outlook.com>
2021-11-11 06:39:49 +0800

    init project

---
+++ src/test/java/org/fairysoftw/fairyhr/FairyhrApplicationTests.java
@@ -0,0 +1,13 @@
+package org.fairysoftw.fairyhr;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FairyhrApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
