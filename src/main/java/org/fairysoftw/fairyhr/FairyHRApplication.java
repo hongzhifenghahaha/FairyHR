7236796bd0df41257a2ae463ac9c6efbdae040ef
86183 <1979557180@qq.com>
2021-11-22 09:26:30 +0800

    add: Add jsp dependency based on the latest version of the master branch

--- src/main/java/org/fairysoftw/fairyhr/FairyHRApplication.java
+++ src/main/java/org/fairysoftw/fairyhr/FairyHRApplication.java
@@ -1,16 +1,29 @@
 package org.fairysoftw.fairyhr;
 
+import lombok.extern.slf4j.Slf4j;
 import org.mybatis.spring.annotation.MapperScan;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.autoconfigure.security.servlet.SecurityAutoConfiguration;
+import org.springframework.context.ConfigurableApplicationContext;
+import org.springframework.core.env.Environment;
+import org.springframework.util.StringUtils;
 
+import java.lang.management.ManagementFactory;
+import java.net.InetAddress;
+import java.net.UnknownHostException;
+
+@Slf4j
 @SpringBootApplication(exclude = {SecurityAutoConfiguration.class})
 @MapperScan("org.fairysoftw.fairyhr.mapper")
 public class FairyHRApplication {
 
-    public static void main(String[] args) {
-        SpringApplication.run(FairyHRApplication.class, args);
-    }
+	public static void main(String[] args) throws UnknownHostException {
+		SpringApplication.run(FairyHRApplication.class, args);
+		log.info("\n----------------------------------------------------------\n\t" +
+				"Application  is running! Access URLs:\n\t" +
+				"Local访问网址: http://localhost:" + 8080 + "\n" +
+				"----------------------------------------------------------");
+	}
 
 }
--- src/main/java/org/fairysoftw/fairyhr/controller/HomeController.java
+++ src/main/java/org/fairysoftw/fairyhr/controller/HomeController.java
@@ -1,12 +1,16 @@
 package org.fairysoftw.fairyhr.controller;
 
+import org.springframework.stereotype.Controller;
 import org.fairysoftw.fairyhr.service.HomeService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.servlet.ModelAndView;
 
-@RestController
+import java.io.IOException;
+
+@Controller
 @RequestMapping("/")
 public class HomeController {
     private HomeService homeService;
@@ -19,6 +23,7 @@ public class HomeController {
     @GetMapping("/")
     public String getHome()
     {
-        return homeService.getSchedulesString().get(0);
+        return "index";
+        //homeService.getSchedulesString().get(0);
     }
 }
