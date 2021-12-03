c703cccec866e2f131f2fc7e169ed259f7513d9b
lxy <852559563@qq.com>
2021-12-04 00:32:57 +0800

    merge origin/master

--- src/main/java/org/fairysoftw/fairyhr/controller/HomeController.java
+++ src/main/java/org/fairysoftw/fairyhr/controller/HomeController.java
@@ -1,23 +1,66 @@
 package org.fairysoftw.fairyhr.controller;
 
-import org.fairysoftw.fairyhr.model.Department;
+import org.fairysoftw.fairyhr.model.User;
 import org.springframework.stereotype.Controller;
+import org.fairysoftw.fairyhr.service.HomeService;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 import org.springframework.web.servlet.ModelAndView;
 
+import javax.servlet.http.Cookie;
 import java.io.IOException;
-import java.util.List;
 
 @Controller
 @RequestMapping("/")
 public class HomeController {
+    private final UserService userService;
+    private final DepartmentService departmentService;
+
+    @Autowired
+    HomeController(UserService userService, DepartmentService departmentService){
+        this.userService = userService;
+        this.departmentService = departmentService;
+    }
+
+    @Autowired
+    public HomeController(HomeService homeService) {
+        this.homeService = homeService;
+    }
+
     @GetMapping("/")
     public String getHome()
     {
-        return "index";
+        return "home";
         //homeService.getSchedulesString().get(0);
+    }
+
+    @RequestMapping(value = "login",method = RequestMethod.GET)
+    public String getLoginPage(){
+        return "login";
+    }
+
+    @RequestMapping(value = "login",method = RequestMethod.POST)
+    public String proccessLogin(@RequestParam(value = "userid") String userid,
+                                @RequestParam(value = "password") String password){
+
+        User user =  userService.selectById(id);
+
+        if (user!=null && user.getPassword()!=null){
+            if(user.getPassword().equals(password)){
+                //todo:直接存user到session似乎不太好
+                session.setAttribute("user",user);
+                session.setAttribute("id",id);
+                session.setAttribute("userName",user.getName());
+                session.setAttribute("position",user.getPosition());
+                Cookie cookieId = new Cookie("id", id);
+                Cookie cookieName = new Cookie("userName", user.getName());
+                cookieId.setMaxAge(60*60*2);
+                cookieName.setMaxAge(60*60*2);
+                response.addCookie(cookieId);
+                response.addCookie(cookieName);
+                return "homePage";
+            }
+        }
+        return "home";
     }
 }
