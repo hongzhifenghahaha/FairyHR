e897c86f5f40cd3f8433bc4033e0f8fd9e5f4145
ColourfulBlack <kalicock@outlook.com>
2021-12-05 21:26:06 +0800

    department管理权限拦截器

--- src/main/java/org/fairysoftw/fairyhr/interceptor/ManagerInterceptor.java
+++ src/main/java/org/fairysoftw/fairyhr/interceptor/ManagerInterceptor.java
@@ -5,10 +5,12 @@ import org.fairysoftw.fairyhr.service.UserService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
 import org.springframework.web.servlet.HandlerInterceptor;
+import org.springframework.web.servlet.HandlerMapping;
 import org.springframework.web.servlet.ModelAndView;
 
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
+import java.util.Map;
 
 @Component
 public class ManagerInterceptor implements HandlerInterceptor {
@@ -23,15 +25,38 @@ public class ManagerInterceptor implements HandlerInterceptor {
 
     @Override
     public boolean preHandle(HttpServletRequest request, HttpServletResponse response, Object handler) throws Exception {
-        var id = (String) request.getSession().getAttribute("id");
-        var user = userService.selectById(id);
-        if (user != null) {
-            var departments = departmentService.selectByUserId(id);
-            return departments.get(0).getManagers().stream().anyMatch((u) -> u.getId().equals(user.getId()));
-        }
-        else {
+        Map pathVariables = (Map) request.getAttribute(HandlerMapping.URI_TEMPLATE_VARIABLES_ATTRIBUTE);
+        var user_id = (String) request.getSession().getAttribute("id");
+        var department_id = (String) pathVariables.get("id");
+        var user = userService.selectById(user_id);
+        if (user == null) {
             response.sendRedirect(request.getContextPath() + "/login");
             return false;
+        }
+        if (department_id == null) {
+            var departments = departmentService.selectByUserId(user.getId());
+            for (var department : departments) {
+                if (department.getManagers().stream().anyMatch((u) -> u.getId().equals(user.getId()))) {
+                    return true;
+                }
+            }
+            response.sendRedirect(request.getContextPath() + "/login");
+            return false;
+        }
+        else {
+            var department = departmentService.selectById(department_id);
+            if (department != null) {
+                if(department.getManagers() != null && department.getManagers().stream().anyMatch((u) -> u.getId().equals(user.getId()))) {
+                    return true;
+                } else {
+                    response.sendRedirect(request.getContextPath() + "/department");
+                    return false;
+                }
+            }
+            else {
+                response.sendRedirect(request.getContextPath() + "/department");
+                return false;
+            }
         }
     }
 
